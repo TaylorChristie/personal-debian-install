5508499eff904eabf3688e36f9d226a410e4418a
Taylor <7576958+TaylorChristie@users.noreply.github.com>
2018-09-28 12:50:32 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,10 @@
 
 cp .bashrc ~/.bashrc
 
+# install docker, docker-compose, make, sponge, build-essential, vscode, chromium
+
+# install conky and configs
+
+# install window manager and themes
+
+# generate SSH keys and output public key
